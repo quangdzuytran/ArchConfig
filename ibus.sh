18d966b34a5a62454351d97a793e9e97cc31a4f1
Quang-Dzuy Tran <quangduy0412@gmail.com>
2021-12-31 12:01:19 +0700

    Init

---
+++ ibus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS=@im=ibus
+export QT4_IM_MODULE=ibus
+export CLUTTER_IM_MODULE=ibus
+ibus-daemon -drxR
---
+++ ssh-agent-startup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+[ -n "$SSH_AGENT_PID" ] || eval "$(ssh-agent -s)"
